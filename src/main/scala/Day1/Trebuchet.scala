8b3c1b344528d79bc30a7c1b606f63ac09f223b3
nagini s <nagini18@gmail.com>
2023-12-11 10:01:47 -0500

    Day 1 - Task 1 ,2

--- src/main/scala/Day1/Trebuchet.scala
+++ src/main/scala/Day1/Trebuchet.scala
@@ -1,5 +1,7 @@
 package Day1
 
+import scala.util.matching.Regex
+
 object Trebuchet extends App{
 
   val regexpart1 = """\d+""".r
@@ -14,4 +16,29 @@ object Trebuchet extends App{
 }.sum
 println(part1)
 
+  def parseWordsToNumbers(value: String): Int = value match {
+    case "one" => 1
+    case "two" => 2
+    case "three" => 3
+    case "four" => 4
+    case "five" => 5
+    case "six" => 6
+    case "seven" => 7
+    case "eight" => 8
+    case "nine" => 9
+    case d => d.charAt(0) - '0'
+  }
+
+  var input = io.Source
+    .fromResource("input.txt")
+    .getLines.toList
+  def firstDigitInString(line: String, regex: Regex): Option[Int] = regex.findFirstIn(line).map(parseWordsToNumbers)
+
+  def lastDigitInString(line: String, regex: Regex): Int = line.indices.map( line.substring).flatMap(firstDigitInString(_, regex)).last
+
+  def CalculatedcalibrationValue(line: String, regex: Regex): Int = 10 * firstDigitInString(line, regex).get + lastDigitInString(line, regex)
+
+  val part2 = input.map(CalculatedcalibrationValue(_, """one|two|three|four|five|six|seven|eight|nine|\d""".r)).sum
+
+  println(part2)
 }
